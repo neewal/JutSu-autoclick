3a8bc2607174c4b11ca15833ffc8763402356a0c
Anom-Anonim <60498785+Anom-Anonim@users.noreply.github.com>
2022-08-30 17:21:25 +0300

    0.1
    AutoClick in Jut.su

---
+++ main.py
@@ -0,0 +1,129 @@
+from vosk import Model, KaldiRecognizer  # оффлайн-распознавание от Vosk
+import speech_recognition  # распознавание пользовательской речи (Speech-To-Text)
+import pyttsx3  # синтез речи (Text-To-Speech)
+import wave  # создание и чтение аудиофайлов формата wav
+import json  # работа с json-файлами и json-строками
+import os  # работа с файловой системой
+import os
+import schedule
+import pyautogui as pg
+import pyautogui
+pyautogui.FAILSAFE = False
+import keyboard
+import time
+from threading import Thread
+from time import sleep
+
+
+class VoiceAssistant:
+    """
+    Настройки голосового ассистента, включающие имя, пол, язык речи
+    """
+    name = "See"
+    sex = "man"
+    speech_language = "ru"
+    recognition_language = "ru"
+
+
+def setup_assistant_voice():
+    """
+    Установка голоса по умолчанию (индекс может меняться в
+    зависимости от настроек операционной системы)
+    """
+    voices = ttsEngine.getProperty("voices")
+
+    if assistant.speech_language == "en":
+        assistant.recognition_language = "en-US"
+        if assistant.sex == "female":
+            # Microsoft Zira Desktop - English (United States)
+            ttsEngine.setProperty("voice", voices[1].id)
+        else:
+            # Microsoft David Desktop - English (United States)
+            ttsEngine.setProperty("voice", voices[2].id)
+    else:
+        assistant.recognition_language = "ru-RU"
+        # Microsoft Irina Desktop - Russian
+        ttsEngine.setProperty("voice", voices[0].id)
+
+
+def play_voice_assistant_speech(text_to_speech):
+    """
+    Проигрывание речи ответов голосового ассистента (без сохранения аудио)
+    :param text_to_speech: текст, который нужно преобразовать в речь
+    """
+    ttsEngine.say(str(text_to_speech))
+    ttsEngine.runAndWait()
+
+
+def record_and_recognize_audio(*args: tuple):
+    """
+    Запись и распознавание аудио
+    """
+    with microphone:
+        recognized_data = ""
+
+        # регулирование уровня окружающего шума
+        recognizer.adjust_for_ambient_noise(microphone, duration=2)
+
+
+        print("Listening...")
+        audio = recognizer.listen(microphone)
+
+
+        # использование online-распознавания через Google
+        # (высокое качество распознавания)
+        try:
+            print("Started recognition...")
+            recognized_data = recognizer.recognize_google(audio, language="ru").lower()
+
+        except speech_recognition.UnknownValueError:
+            pass
+
+        # в случае проблем с доступом в Интернет происходит
+        # попытка использовать offline-распознавание через Vosk
+        except speech_recognition.RequestError:
+            print("Trying to use offline recognition...")
+
+        return recognized_data
+
+
+if __name__ == "__main__":
+
+    # инициализация инструментов распознавания и ввода речи
+    recognizer = speech_recognition.Recognizer()
+    microphone = speech_recognition.Microphone()
+
+    # инициализация инструмента синтеза речи
+    ttsEngine = pyttsx3.init()
+
+    # настройка данных голосового помощника
+    assistant = VoiceAssistant()
+    assistant.name = "Alice"
+    assistant.sex = "female"
+    assistant.speech_language = "ru"
+
+    # установка голоса по умолчанию
+    setup_assistant_voice()
+
+    while True:
+        # старт записи речи с последующим выводом распознанной речи
+        # и удалением записанного в микрофон аудио
+        voice_input = record_and_recognize_audio()
+
+        # отделение комманд от дополнительной информации (аргументов)
+        voice_input = voice_input.split(" ")
+        command = voice_input[0]
+        print(voice_input)
+        if command == "next":
+            pg.leftClick(1832, 1005)
+            sleep(2)
+            pg.leftClick(845, 717)
+            sleep(2)
+            pg.leftClick(1093, 858)
+        if command == "пропустить":
+            pg.leftClick(90, 1008)
+        if command == "привет":
+            play_voice_assistant_speech("Здравствуй")
+        if command == "пока":
+            pg.leftClick(1832, 1005)
+
